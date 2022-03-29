91fecb6c782982d5d029c36fcc62e76578a9c974
sairam <sairams6190@gmail.com>
2022-03-29 07:40:32 +0530

    first commit

---
+++ src/main/java/com/example/demo/Employee.java
@@ -0,0 +1,62 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Employee_test")
+public class Employee {
+	@Id
+	private int id;
+	private String name;
+	private int salary;
+	private String designation;
+	
+	public Employee() {
+		
+	}
+
+	public Employee(int id, String name, int salary, String designation) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+		this.designation = designation;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	public String getDesignation() {
+		return designation;
+	}
+
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+	
+	
+
+}
